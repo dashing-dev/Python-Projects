29b3a39c8c5d1f9e1aa305b0b200663cbf34d506
Sanjay Bhandari <93391951+dashing-dev@users.noreply.github.com>
2022-02-02 12:02:29 +0545

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,3 +1,7 @@
 #--------first program-------------
 name = input("enter your name :")
 print("hello  " + name)
+#-----------third program------------
+age = 14
+age += 1 
+print( "your age is " +str(age))
