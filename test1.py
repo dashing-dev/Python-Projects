3f7d8f2c453d3092d3ae7b73ddfce8f141bf96e6
Sanjay Bhandari <93391951+dashing-dev@users.noreply.github.com>
2022-02-09 12:32:53 +0545

    Update test1.py

--- test1.py
+++ test1.py
@@ -5,3 +5,11 @@ print("hello  " + name)
 age = 14
 age += 1 
 print( "your age is " +str(age))
+#------------logical operators------
+temp = int(input("what is the temperature outside?"))
+if temp >= 0 and temp <= 30:
+    print("the temperature is good outside")
+    print("go outside")
+elif  temp < 0 or  temp > 30:
+    print("weather is bad outside")
+    print("stay inside")
